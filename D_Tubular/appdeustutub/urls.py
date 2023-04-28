ecef08efb2df7dfbdbce7383fe3098f4939a2774
Rodrigo <rod.ruizcuevas@opendeusto.es>
2023-04-28 18:57:07 +0100

    comments+index_equipo+view

--- D_Tubular/appdeustutub/urls.py
+++ D_Tubular/appdeustutub/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
  path('', views.index, name='index'),
+ path('equipos/', views.index_equipo, name='index_equipo')
 ]
--- D_Tubular/appdeustutub/views.py
+++ D_Tubular/appdeustutub/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Empleado, Equipo, Proceso
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 
 def index(request):
     #           referenciar el título de la página
-    context = {'title_page': 'Seleccione una opción:'}
-    return render(request, 'index.html', context)
+    # context = {'title_page': 'Seleccione una opción:'}
+    return render(request, 'index.html')
+
+# muestra los equipos
+def index_equipo(request):
+	equipos = Equipo.objects.order_by('marca')    
+    # context = {'title_page': ' ', 'marca' }
+	context = {'lista_equipos': equipos}
+	return render(request, 'index_equipo.html', context)
