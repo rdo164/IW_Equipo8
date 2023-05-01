05213d94c318fc8d52ddd95b5cda149ef8356ef5
asalazar21 <131751105+asalazar21@users.noreply.github.com>
2023-05-01 19:22:53 +0200

    completo

--- IW_Grupo8/appEmpresaGrupo8/admin.py
+++ IW_Grupo8/appEmpresaGrupo8/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Equipo, Proceso, Empleado
 
 # Register your models here.
+
+admin.site.register(Equipo)
+admin.site.register(Proceso)
+admin.site.register(Empleado)
