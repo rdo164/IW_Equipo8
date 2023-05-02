5664681f7533242e9324046af86dd9a1154ba021
Álex Cue Ruiz <alex.cue@opendeusto.es>
2023-05-02 17:48:52 +0200

    Update del proyecto

--- appEmpresaGrupo8/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Equipo, Proceso, Empleado
-
-# Register your models here.
-
-admin.site.register(Equipo)
-admin.site.register(Proceso)
-admin.site.register(Empleado)
--- appEmpresaGrupo8/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Appempresagrupo8Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'appEmpresaGrupo8'
